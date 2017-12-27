d6801d58f8202dfab85a0d1f0e91dc21c59a6fd5
2GIS\a.rasskazov <a.rasskazov@2gis.ru>
2017-12-27 17:17:53 +0700

    Добавил обработку входных параметров с помощью библиотеки CommandLine

---
+++ Options.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace ThreadsBattle
+{
+    public class Options
+    {
+        [Option("n", Required = true, HelpText = "Number of added threads.")]
+        public int N { get; set; }
+
+        [Option("m", Required = true, HelpText = "Number of taken threads.")]
+        public int M { get; set; }
+
+        [Option("a", Required = true, HelpText = "Minimum possible random number added in queue.")]
+        public int A { get; set; }
+
+        [Option("b", Required = true, HelpText = "Maximum possible random number added in queue.")]
+        public int B { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText();
+            help.AddPreOptionsLine("Threads battle");
+            help.AddOptions(this);
+            help.AddPostOptionsLine(@"Examples:");
+            help.AddPostOptionsLine(@"-n 5 -m 15 -a 0 -b 100");
+            return help;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,74 +7,50 @@ namespace ThreadsBattle
 {
     public class Program
     {
-        private static int N;
-        private static int A;
-        private static int B;
-        private static int M;
         private static readonly Random Random = new Random(Guid.NewGuid().GetHashCode());
         private static readonly Action<int> RandomSleepAction = x => Thread.Sleep(Random.Next(x));
         private const int MaxSleepingTime = 1000;
 
         public static void Main(string[] args)
         {
-           ParseArgs(args);
-            var queue = new ConcurrentQueue<long>();
+            var options = new Options();
 
-            AsyncAddRandomNumbersInQueue(queue, N);
-            AsyncTakeNumbersFromQueueInThreadAndDetermineIsPrime(queue, M);
+            if(CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                var queue = new ConcurrentQueue<int>();
+
+                AsyncAddRandomNumbersInQueue(queue, options.N, options.A, options.B);
+                AsyncTakeNumbersFromQueueInThreadAndDetermineIsPrime(queue, options.M);
+            }
             Console.ReadKey();
         }
 
-        private static void ParseArgs(string[] args)
-        {
-            try
-            {
-                N = 2;//Convert.ToInt32(args[0]);
-                M = 10;//Convert.ToInt32(args[1]);
-                A = 0;//Convert.ToInt32(args[2]);
-                B = 10; //Convert.ToInt32(args[3]);
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-                Console.WriteLine("Заданы не все входные параметры");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Входные параметры должны быть целыми числами");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Неверно заданы входные параметры");
-            }
-
-        }
-
-        private static void AsyncTakeNumbersFromQueueInThreadAndDetermineIsPrime(ConcurrentQueue<long> queue, int m)
+        private static void AsyncTakeNumbersFromQueueInThreadAndDetermineIsPrime(ConcurrentQueue<int> queue, int m)
         {
             Parallel.For(0, m, x => { Task.Run(() => TakeNumberFromQueueAndDetermineIsPrime(queue)); });
         }
 
-        private static void AsyncAddRandomNumbersInQueue(ConcurrentQueue<long> queue, long n)
+        private static void AsyncAddRandomNumbersInQueue(ConcurrentQueue<int> queue, int n, int a, int b)
         {
-            Parallel.For(0, n, x => { Task.Run(() => AddRandomNumberToQueue(queue)); });
+            Parallel.For(0, n, x => { Task.Run(() => AddRandomNumberToQueue(queue, a, b)); });
         }
 
-        private static void AddRandomNumberToQueue(ConcurrentQueue<long> queue)
+        private static void AddRandomNumberToQueue(ConcurrentQueue<int> queue, int a, int b)
         {
             while (true)
             {
                 RandomSleepAction(MaxSleepingTime);
-                queue.Enqueue(Random.Next(A, B));
+                queue.Enqueue(Random.Next(a, b));
             }
         }
 
-        private static void TakeNumberFromQueueAndDetermineIsPrime(ConcurrentQueue<long> queue)
+        private static void TakeNumberFromQueueAndDetermineIsPrime(ConcurrentQueue<int> queue)
         {
             while (true)
             {
                 RandomSleepAction(MaxSleepingTime);
 
-                long number;
+                int number;
 
                 if (queue.TryDequeue(out number))
                 {
@@ -83,7 +59,7 @@ namespace ThreadsBattle
             }
         }
 
-        private static void WriteIsPrimeNumber(long number)
+        private static void WriteIsPrimeNumber(int number)
         {
             Console.WriteLine(Calculating.IsPrime(number) ? $"Число {number} простое" : $"Число {number} не простое");
         }
